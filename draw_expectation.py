0259f7fa1b6f8f858a1c19668907610d2a0cffb5
Natalie Kharchenko <smiledragon93@gmail.com>
2017-01-17 16:18:35 +0100

    Now picture looks not very bad

--- draw_expectation.py
+++ draw_expectation.py
@@ -4,17 +4,21 @@ from pylab import *
 from draw_lattice import *
 from arbitrary_lattice_gaussian import *
 
-def create_box(x_1, x_2, y_2, s, width, height, precision, name):
+def create_box(x_1, x_2, y_2, s, num_x, num_y, precision, name):
+    width = (num_x + 1) * x_1
+    height = num_y * y_2
     x = np.arange(0, width, precision)
     y = np.arange(0, height, precision)
     X, Y = meshgrid(x, y)
     Z = exp_dist_lattice(x_1, x_2, y_2, X, Y, s, 100)
     fig, ax = subplots()
     cnt = contour(Z, cmap=cm.RdBu,vmin=abs(Z).min(), vmax=abs(Z).max(), extent=[0, width, 0, height])
-    (X,Y) = generate_lattice(x_1, x_2, y_2, 4, 3)
+    (X,Y) = generate_lattice(x_1, x_2, y_2, num_x, num_y)
     plt.scatter(X,Y)
     plt.grid(True)
-    plt.axis('off')
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.xticks(arange(width + 1))
+    plt.yticks(arange(height + 1))
     plt.show()
     plt.savefig(name, bbox_inches='tight', pad_inches=0)
 
